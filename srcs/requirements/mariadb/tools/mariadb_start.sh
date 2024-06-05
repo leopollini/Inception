f08ad2af14792a2764fc95002bb475dab9d0ca2a
leopollini <leopollinik@gmail.com>
2024-06-05 07:05:02 -0500

    Def

---
+++ srcs/requirements/mariadb/tools/mariadb_start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "CREATE DATABASE IF NOT EXISTS wordpress;" > /var/www/initial_db.sql
+echo "CREATE USER IF NOT EXISTS "$DB_USER_LOGIN"@'%' IDENTIFIED BY $DB_USER_PASSWORD;" > /var/www/initial_db.sql
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO "$DB_USER_LOGIN"@'%';" > /var/www/initial_db.sql
+echo "FLUSH PRIVILEGES;" > /var/www/initial_db.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY "$DB_ROOT_PASSWORD > /var/www/initial_db.sql
