4b8f6f28f8872c57b3ced698764ba7d27b585f99
leopollini <leopollinik@gmail.com>
2024-06-05 11:10:09 -0500

    final

--- srcs/requirements/mariadb/tools/mariadb_start.sh
+++ srcs/requirements/mariadb/tools/mariadb_start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+
 echo "CREATE DATABASE IF NOT EXISTS wordpress;" > /var/www/initial_db.sql
-echo "CREATE USER IF NOT EXISTS "$DB_USER_LOGIN"@'%' IDENTIFIED BY $DB_USER_PASSWORD;" > /var/www/initial_db.sql
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO "$DB_USER_LOGIN"@'%';" > /var/www/initial_db.sql
-echo "FLUSH PRIVILEGES;" > /var/www/initial_db.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY "$DB_ROOT_PASSWORD > /var/www/initial_db.sql
+echo "CREATE USER IF NOT EXISTS "$DB_USER_LOGIN"@'%' IDENTIFIED BY $DB_USER_PASSWORD;" >> /var/www/initial_db.sql
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO "$DB_USER_LOGIN"@'%';" >> /var/www/initial_db.sql
+echo "FLUSH PRIVILEGES;" >> /var/www/initial_db.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY "$DB_ROOT_PASSWORD >> /var/www/initial_db.sql
+
+service mysql start && mysql < /var/www/initial_db.sql && rm -f /var/www/initial_db.sql;
+
+exec "$@";
